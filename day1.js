acde24f258db0a365f75e4e2c95366ba6a7dc761
mustapha moutaki <mustaphaamoutaki@gmail.com>
2025-03-25 15:06:26 +0100

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,21 @@
+let arr=['a', 'aa', 'aaaa', 'aaa'];
+let arrLenght = arr.length;
+for(let i=0; i<arrLenght;i++){
+   for(let j=1; j<arrLenght-1;j++){
+    if(arr[i].length<arr[j].length){
+
+    }
+   }
+}
+
+
+//ex2
+let arr1= [1,0,2,3,4];
+let arr2=[3,5,6,7,8,13];
+
+let arr1Lenght= arr1.length;
+let arr2Lenght = arr2.length;
+
+for(let i=0; i<arr1.length; i++){
+    let arr3 = arr1[i]+arr2[i];
+}
